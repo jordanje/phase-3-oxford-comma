6a3bd986c11293526b17a1cc8667dc4e952969b8
Jordan Jennings <jordanjennings12@gmail.com>
2022-09-21 16:34:07 -0400

    commit tree a59cd15567ba5aaea9477ba15cc5c99c300758e5

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    return array.join(" and ") if array.size < 3
+    
+    index = array.length - 1
+    newArr = []
+    array.insert(index, "and")
+    array.each do |i|
+        if i != "and" && i != array.last()
+            newArr.push(i + ",")
+        else
+            newArr << i
+        end
+    end
+
+ return newArr.join(" ")
 end
+
+
+
+binding.pry
